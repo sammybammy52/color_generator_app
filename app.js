49627cf67fca3d2b7a44cdf90a0b565f301c3284
Sammy Bammy <sammybammystudios@gmail.com>
2021-12-17 11:55:54 +0100

    placing this stuff here

---
+++ app.js
@@ -0,0 +1,15 @@
+const colors = ["green", "red", "orange", "blue"]
+
+const btn =  document.getElementById('btn');
+const color = document.querySelector(".color");
+const container = document.getElementById('con');
+
+btn.addEventListener("click", function(){
+    const randomNumber = getRandomNumber();
+    console.log(randomNumber);
+    container.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+})
+function getRandomNumber(){
+    return Math.floor(Math.random() * colors.length);
+}
